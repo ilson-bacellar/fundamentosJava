42fad5b60595d71dfd98377890ab6ddd843ba20e
ilson-bacellar <ilsonbacellar@yahoo.com.br>
2022-10-28 22:10:31 -0300

    segundo push

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/37/d0bde1282657001d11b0d921a6086559
@@ -0,0 +1,5 @@
+package br.edu.infnet.app.testes;
+
+public class Primeiro {
+
+}
--- AppIlsonBacellarTP1/src/br/edu/infnet/app/testes/Primeiro.java
+++ AppIlsonBacellarTP1/src/br/edu/infnet/app/testes/Primeiro.java
@@ -1,5 +1,10 @@
 package br.edu.infnet.app.testes;
 
 public class Primeiro {
+	
+	//Segunda tentativa de push
+	public static void main(String[] args) {
+		
+	}
 
 }
